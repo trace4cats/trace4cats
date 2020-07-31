35757f6554b963c031959e0c9cdf27c342d2207d
catostrophe <40268503+catostrophe@users.noreply.github.com>
2020-07-31 13:06:13 +0300

    namer function existentiality reworked

--- modules/http4s-common/src/main/scala/io/janstenpickle/trace4cats/http4s/common/package.scala
+++ modules/http4s-common/src/main/scala/io/janstenpickle/trace4cats/http4s/common/package.scala
@@ -3,18 +3,10 @@ package io.janstenpickle.trace4cats.http4s
 import org.http4s.Request
 
 package object common {
-
+  type AnyK[_] = Any
   /**
-    * `Http4sSpanNamer` is intentionally existential, since no knowledge about `F[_]`
+    * `Http4sSpanNamer` is intentionally existential via `AnyK`, since no knowledge about `F[_]`
     * may be used to get a `String` from a `Request[F]`.
-    *
-    * Note on cross-compiling with Scala 3: `forSome` types are no longer supported,
-    * but the wildcard has become poly-kinded and should be used instead.
-    *
-    * The proper definition for Scala 3:
-    * {{{
-    *   type Http4sSpanNamer = Request[_] => String
-    * }}}
     */
-  type Http4sSpanNamer = (Request[f] forSome { type f[_] }) => String
+  type Http4sSpanNamer = Request[AnyK] => String
 }
