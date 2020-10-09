9034e6b39cac11ed0ca8401a82ade9a2430c4e58
janstenpickle <chris.jansen@permutive.com>
2020-10-09 22:49:44 +0100

    Fix build

--- modules/opentelemetry-common/src/main/scala/io/janstenpickle/trace4cats/opentelemetry/common/Trace4CatsResource.scala
+++ modules/opentelemetry-common/src/main/scala/io/janstenpickle/trace4cats/opentelemetry/common/Trace4CatsResource.scala
@@ -1,19 +1,21 @@
 package io.janstenpickle.trace4cats.opentelemetry.common
 
+import io.janstenpickle.trace4cats.model.AttributeValue.StringValue
 import io.janstenpickle.trace4cats.model.TraceProcess
-import io.opentelemetry.common.{AttributeValue, Attributes}
+import io.opentelemetry.common.Attributes
 import io.opentelemetry.sdk.resources.Resource
 
 object Trace4CatsResource {
   final val ServiceNameResourceKey = "service.name"
 
-  def apply(process: TraceProcess): Resource = {
-    val attrs = toAttributes[Attributes.Builder](
-      process.attributes,
-      Attributes.newBuilder(),
-      (builder, k, v) => builder.setAttribute(k, v)
-    ).setAttribute(ServiceNameResourceKey, AttributeValue.stringAttributeValue(process.serviceName))
-
-    Resource.create(attrs.build())
-  }
+  def apply(process: TraceProcess): Resource =
+    Resource.create(
+      Attributes
+        .newBuilder(
+          Trace4CatsReadableAttributes(
+            process.attributes + (ServiceNameResourceKey -> StringValue(process.serviceName))
+          )
+        )
+        .build()
+    )
 }
