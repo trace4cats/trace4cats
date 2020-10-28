33716231c9b1d50ac7618c20baca731a3fd07251
Scala Steward <me@scala-steward.org>
2020-10-28 22:31:04 +0100

    Update circe-yaml to 0.13.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val catsEffect = "2.2.0"
     val collectionCompat = "2.2.0"
     val commonsCodec = "1.15"
-    val circe = "0.13.0"
+    val circe = "0.13.1"
     val decline = "1.3.0"
     val enumeratum = "1.6.1"
     val fs2 = "2.4.4"
