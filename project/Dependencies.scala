ed8218d19823bb9cf9aac964879cf615e41be8f1
Scala Steward <me@scala-steward.org>
2020-11-13 00:31:54 +0100

    Update discipline-scalatest to 2.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -38,7 +38,7 @@ object Dependencies {
     val vulcan = "1.2.0"
     val zioInterop = "2.2.0.1"
 
-    val disciplineScalatest = "2.0.1"
+    val disciplineScalatest = "2.1.0"
     val discipline = "1.1.2"
     val scalaCheck = "1.15.1"
     val scalaCheckShapeless = "1.2.5"
