12b61c7d2d95a45e9dd2ed8abad796ddfac19c7a
catostrophe <40268503+catostrophe@users.noreply.github.com>
2020-11-18 12:59:01 +0300

    fix SttpHeaders.converter, add tests

--- modules/sttp-client/src/main/scala/io/janstenpickle/trace4cats/sttp/backend/SttpHeaders.scala
+++ modules/sttp-client/src/main/scala/io/janstenpickle/trace4cats/sttp/backend/SttpHeaders.scala
@@ -6,7 +6,7 @@ import sttp.model.{Header, Headers}
 object SttpHeaders {
   val converter: TraceHeaders.Converter[Headers] = new TraceHeaders.Converter[Headers] {
     def from(t: Headers): TraceHeaders =
-      TraceHeaders(t.headers.map(h => h.value -> h.name).toMap)
+      TraceHeaders(t.headers.map(h => h.name -> h.value).toMap)
     def to(h: TraceHeaders): Headers =
       Headers(h.values.map { case (k, v) => Header(k, v) }.toList)
   }
