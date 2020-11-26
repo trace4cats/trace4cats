cf987d2a29f847aa3264f3ea081161ef556821e1
Scala Steward <me@scala-steward.org>
2020-11-26 04:56:47 +0100

    Update http4s-blaze-client, ... to 0.21.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
     val googleCredentials = "0.22.0"
     val googleCloudTrace = "1.2.7"
     val grpc = "1.33.1"
-    val http4s = "0.21.12"
+    val http4s = "0.21.13"
     val http4sJdkClient = "0.3.1"
     val jaeger = "1.5.0"
     val jwt = "3.11.0"
