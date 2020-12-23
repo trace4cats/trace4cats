b07d2fe21ce416065a5c32ba872b2e51f9c2c030
catostrophe <40268503+catostrophe@users.noreply.github.com>
2020-12-23 08:55:12 +0300

    remove object method

--- modules/base/src/main/scala/io/janstenpickle/trace4cats/base/context/Provide.scala
+++ modules/base/src/main/scala/io/janstenpickle/trace4cats/base/context/Provide.scala
@@ -32,19 +32,4 @@ trait Provide[Low[_], F[_], R] extends Local[F, R] with Unlift[Low, F] { self =>
 
 object Provide {
   def apply[Low[_], F[_], R](implicit ev: Provide[Low, F, R]): Provide[Low, F, R] = ev
-
-  def imapK[Low[_]: Monad, F[_], G[_]: Monad, R](self: Provide[Low, F, R], fk: F ~> G, gk: G ~> F): Provide[Low, G, R] =
-    new Provide[Low, G, R] {
-      override def provide[A](fa: G[A])(r: R): Low[A] = self.provide(gk(fa))(r)
-
-      override def local[A](fa: G[A])(f: R => R): G[A] = fk(self.local(gk(fa))(f))
-
-      override def Low: Monad[Low] = implicitly
-
-      override def F: Monad[G] = implicitly
-
-      override def lift[A](la: Low[A]): G[A] = fk(self.lift(la))
-
-      override def ask[R1 >: R]: G[R1] = fk(self.ask[R1])
-    }
 }
