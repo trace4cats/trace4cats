e77bb70e8cbd25b3e883216369adffffc5e3867f
catostrophe <40268503+catostrophe@users.noreply.github.com>
2020-12-23 08:52:23 +0300

    add mapK/imapK to context typeclasses

--- modules/base/src/main/scala/io/janstenpickle/trace4cats/base/context/Provide.scala
+++ modules/base/src/main/scala/io/janstenpickle/trace4cats/base/context/Provide.scala
@@ -2,7 +2,7 @@ package io.janstenpickle.trace4cats.base.context
 
 import cats.{~>, Monad}
 
-trait Provide[Low[_], F[_], R] extends Local[F, R] with Unlift[Low, F] {
+trait Provide[Low[_], F[_], R] extends Local[F, R] with Unlift[Low, F] { self =>
   def provide[A](fa: F[A])(r: R): Low[A]
 
   def provideK(r: R): F ~> Low = λ[F ~> Low](provide(_)(r))
@@ -11,17 +11,29 @@ trait Provide[Low[_], F[_], R] extends Local[F, R] with Unlift[Low, F] {
 
   def kleislift[A](f: R => Low[A]): F[A] = accessF(f.andThen(lift))
 
-  def imapK[G[_]: Monad](fk: F ~> G, gk: G ~> F): Provide[Low, G, R] = Provide.imapK(this, fk, gk)
+  override def mapK[G[_]: Monad](fk: F ~> G): Ask[G, R] with Lift[Low, G] =
+    new Ask[G, R] with Lift[Low, G] {
+      def Low: Monad[Low] = self.Low
+      def F: Monad[G] = implicitly
+      def lift[A](la: Low[A]): G[A] = fk(self.lift(la))
+      def ask[R1 >: R]: G[R1] = fk(self.ask[R1])
+    }
+
+  override def imapK[G[_]: Monad](fk: F ~> G, gk: G ~> F): Provide[Low, G, R] =
+    new Provide[Low, G, R] {
+      def Low: Monad[Low] = self.Low
+      def F: Monad[G] = implicitly
+      def lift[A](la: Low[A]): G[A] = fk(self.lift(la))
+      def ask[R1 >: R]: G[R1] = fk(self.ask[R1])
+      def local[A](fa: G[A])(f: R => R): G[A] = fk(self.local(gk(fa))(f))
+      def provide[A](fa: G[A])(r: R): Low[A] = self.provide(gk(fa))(r)
+    }
 }
 
 object Provide {
   def apply[Low[_], F[_], R](implicit ev: Provide[Low, F, R]): Provide[Low, F, R] = ev
 
-  private def imapK[Low[_]: Monad, F[_], G[_]: Monad, R](
-    self: Provide[Low, F, R],
-    fk: F ~> G,
-    gk: G ~> F
-  ): Provide[Low, G, R] =
+  def imapK[Low[_]: Monad, F[_], G[_]: Monad, R](self: Provide[Low, F, R], fk: F ~> G, gk: G ~> F): Provide[Low, G, R] =
     new Provide[Low, G, R] {
       override def provide[A](fa: G[A])(r: R): Low[A] = self.provide(gk(fa))(r)
 
