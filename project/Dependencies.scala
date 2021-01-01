39e781881cbe0efc89f024ced89980b31c357879
Scala Steward <me@scala-steward.org>
2021-01-01 06:49:51 +0100

    Update http4s-jdk-http-client to 0.3.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
     val googleCloudTrace = "1.2.10"
     val grpc = "1.34.1"
     val http4s = "0.21.15"
-    val http4sJdkClient = "0.3.2"
+    val http4sJdkClient = "0.3.3"
     val jaeger = "1.5.0"
     val jwt = "3.12.0"
     val kafka = "2.7.0"
