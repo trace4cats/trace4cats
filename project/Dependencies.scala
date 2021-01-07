eefaaae9ea5da46cd84864ef14cb4e530c58cbf3
Scala Steward <me@scala-steward.org>
2021-01-07 16:42:05 +0100

    Update tapir-cats, tapir-http4s-server, ... to 0.17.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -35,7 +35,7 @@ object Dependencies {
     val sttpClient2 = "2.2.9"
     val sttpClient3 = "3.0.0-RC14"
     val sttpModel = "1.2.0-RC9"
-    val sttpTapir = "0.17.1"
+    val sttpTapir = "0.17.2"
     val svm = "19.2.1"
     val vulcan = "1.2.0"
     val zioInterop = "2.2.0.1"
