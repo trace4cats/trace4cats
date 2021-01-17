993bc2dee4deb168d97416424d06dd839580298c
catostrophe <40268503+catostrophe@users.noreply.github.com>
2021-01-18 00:34:03 +0300

    revert to embedded-kafka consumer

--- modules/avro-kafka-exporter/src/test/scala-2.13/io/janstenpickle/trace4cats/avro/kafka/AvroKafkaSpanExporterSpec.scala
+++ modules/avro-kafka-exporter/src/test/scala-2.13/io/janstenpickle/trace4cats/avro/kafka/AvroKafkaSpanExporterSpec.scala
@@ -4,16 +4,15 @@ import cats.Eq
 import cats.data.NonEmptyList
 import cats.effect.IO
 import cats.syntax.flatMap._
-import cats.syntax.foldable._
 import cats.syntax.show._
 import fs2.Chunk
-import fs2.kafka.{AutoOffsetReset, ConsumerSettings, Deserializer, KafkaConsumer}
 import io.janstenpickle.trace4cats.avro.AvroInstances
-import io.janstenpickle.trace4cats.model.{Batch, CompletedSpan, TraceId}
+import io.janstenpickle.trace4cats.model.{Batch, CompletedSpan}
 import io.janstenpickle.trace4cats.test.ArbitraryInstances
 import net.manub.embeddedkafka.{EmbeddedKafka, EmbeddedKafkaConfig}
 import org.apache.avro.generic.GenericDatumReader
 import org.apache.avro.io.DecoderFactory
+import org.apache.kafka.common.serialization.Deserializer
 import org.scalacheck.Shrink
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
@@ -39,46 +38,25 @@ class AvroKafkaSpanExporterSpec
 
   val schema = AvroInstances.completedSpanCodec.schema.toOption.get
 
-  val keyDeserializer: Deserializer[IO, TraceId] = Deserializer
-    .string[IO]
-    .map(TraceId.fromHexString(_).get)
-    .suspend
+  implicit val deserializer: Deserializer[CompletedSpan] = new Deserializer[CompletedSpan] {
+    override def deserialize(topic: String, data: Array[Byte]): CompletedSpan = {
+      val reader = new GenericDatumReader[Any](schema)
+      val decoder = DecoderFactory.get.binaryDecoder(data, null)
+      val record = reader.read(null, decoder)
 
-  val valueDeserializer: Deserializer[IO, CompletedSpan] = Deserializer.lift { data =>
-    val reader = new GenericDatumReader[Any](schema)
-    val decoder = DecoderFactory.get.binaryDecoder(data, null)
-    val record = reader.read(null, decoder)
-
-    IO(AvroInstances.completedSpanCodec.decode(record, schema).toOption.get)
+      AvroInstances.completedSpanCodec.decode(record, schema).toOption.get
+    }
   }
 
   it should "serialize a batch of spans to kafka" in withRunningKafkaOnFoundPort(userDefinedConfig) {
     implicit actualConfig =>
-      val bootStrapServers = NonEmptyList.one(s"localhost:${actualConfig.kafkaPort}")
-      val topic = "test"
-
       forAll { (batch: Batch[Chunk]) =>
-        val resources = for {
-          exporter <- AvroKafkaSpanExporter[IO, Chunk](bootStrapServers, topic)
-          consumer <- KafkaConsumer
-            .resource[IO]
-            .using(
-              ConsumerSettings[IO, TraceId, CompletedSpan](keyDeserializer, valueDeserializer)
-                .withBootstrapServers(bootStrapServers.mkString_(","))
-                .withGroupId("embedded-kafka-spec")
-                .withAutoOffsetReset(AutoOffsetReset.Earliest)
-                .withEnableAutoCommit(true)
-            )
-            .evalTap(_.subscribeTo(topic))
-        } yield (exporter, consumer)
-
-        val res =
-          resources
-            .use { case (exporter, consumer) =>
-              exporter.exportBatch(batch) >>
-                consumer.stream.take(batch.spans.size.toLong).map(_.record.value).compile.toList
-            }
-            .unsafeRunSync()
+        val res = AvroKafkaSpanExporter[IO, Chunk](NonEmptyList.one(s"localhost:${actualConfig.kafkaPort}"), "test")
+          .use { exporter =>
+            exporter.exportBatch(batch) >>
+              IO(consumeNumberMessagesFrom[CompletedSpan]("test", batch.spans.size))
+          }
+          .unsafeRunSync()
 
         res.size should be(batch.spans.size)
         assert(Eq.eqv(res.sortBy(_.context.spanId.show), batch.spans.toList.sortBy(_.context.spanId.show)))
