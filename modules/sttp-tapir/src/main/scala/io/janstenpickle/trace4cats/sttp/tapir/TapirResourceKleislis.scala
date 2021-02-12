39bb87ece56155642f28592df576546f105e6654
catostrophe <40268503+catostrophe@users.noreply.github.com>
2021-02-12 14:00:20 +0300

    remove errorHandler from Tapir, leave typed E to SpanStatus mapper

--- modules/sttp-tapir/src/main/scala/io/janstenpickle/trace4cats/sttp/tapir/TapirResourceKleislis.scala
+++ modules/sttp-tapir/src/main/scala/io/janstenpickle/trace4cats/sttp/tapir/TapirResourceKleislis.scala
@@ -7,7 +7,7 @@ import cats.effect.{MonadThrow, Resource}
 import io.janstenpickle.trace4cats.Span
 import io.janstenpickle.trace4cats.base.optics.Getter
 import io.janstenpickle.trace4cats.inject.{ResourceKleisli, SpanParams}
-import io.janstenpickle.trace4cats.model.{SpanKind, SpanStatus}
+import io.janstenpickle.trace4cats.model.SpanKind
 import io.janstenpickle.trace4cats.sttp.common.SttpHeaders
 import sttp.model.{HeaderNames, Headers}
 
@@ -17,13 +17,12 @@ object TapirResourceKleislis {
   def fromHeaders[F[_]: Applicative, I](
     inHeadersGetter: Getter[I, Headers],
     inSpanNamer: TapirInputSpanNamer[I],
-    dropHeadersWhen: String => Boolean = HeaderNames.isSensitive,
-    errorHandler: PartialFunction[Throwable, SpanStatus] = PartialFunction.empty,
+    dropHeadersWhen: String => Boolean = HeaderNames.isSensitive
   )(k: ResourceKleisli[F, SpanParams, Span[F]]): ResourceKleisli[F, I, Span[F]] =
     Kleisli { input =>
       val headers = inHeadersGetter.get(input)
       val traceHeaders = SttpHeaders.converter.from(headers)
-      val spanResource = k.run((inSpanNamer(input), SpanKind.Server, traceHeaders, errorHandler))
+      val spanResource = k.run((inSpanNamer(input), SpanKind.Server, traceHeaders, PartialFunction.empty))
 
       spanResource.evalTap(_.putAll(SttpHeaders.requestFields(headers, dropHeadersWhen): _*))
     }
@@ -33,10 +32,9 @@ object TapirResourceKleislis {
     inHeadersGetter: Getter[I, Headers],
     inSpanNamer: TapirInputSpanNamer[I],
     errorToSpanStatus: TapirStatusMapping[E],
-    dropHeadersWhen: String => Boolean = HeaderNames.isSensitive,
-    errorHandler: PartialFunction[Throwable, SpanStatus] = PartialFunction.empty,
+    dropHeadersWhen: String => Boolean = HeaderNames.isSensitive
   )(k: ResourceKleisli[F, SpanParams, Span[F]]): ResourceKleisli[F, I, Either[E, Ctx]] =
-    fromHeaders(inHeadersGetter, inSpanNamer, dropHeadersWhen, errorHandler)(k).tapWithF { (req, span) =>
+    fromHeaders(inHeadersGetter, inSpanNamer, dropHeadersWhen)(k).tapWithF { (req, span) =>
       val fa = EitherT(makeContext(req, span))
         .leftSemiflatTap(e => span.setStatus(errorToSpanStatus(e)))
         .value
@@ -48,10 +46,9 @@ object TapirResourceKleislis {
     inHeadersGetter: Getter[I, Headers],
     inSpanNamer: TapirInputSpanNamer[I],
     errorToSpanStatus: TapirStatusMapping[E],
-    dropHeadersWhen: String => Boolean = HeaderNames.isSensitive,
-    errorHandler: PartialFunction[Throwable, SpanStatus] = PartialFunction.empty,
+    dropHeadersWhen: String => Boolean = HeaderNames.isSensitive
   )(k: ResourceKleisli[F, SpanParams, Span[F]]): ResourceKleisli[F, I, Ctx] =
-    fromHeaders(inHeadersGetter, inSpanNamer, dropHeadersWhen, errorHandler)(k).tapWithF { (req, span) =>
+    fromHeaders(inHeadersGetter, inSpanNamer, dropHeadersWhen)(k).tapWithF { (req, span) =>
       val fa = makeContext(req, span).onError { case e: E =>
         span.setStatus(errorToSpanStatus(e))
       }
