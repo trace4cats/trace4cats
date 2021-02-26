af7186b3db85f1c5b94cc696f30ce065928a22fe
Scala Steward <me@scala-steward.org>
2021-02-26 02:26:54 +0100

    Update google-auth-library-credentials to 0.24.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val enumeratum = "1.6.1"
     val fs2 = "2.5.3"
     val fs2Kafka = "1.4.1"
-    val googleCredentials = "0.24.0"
+    val googleCredentials = "0.24.1"
     val googleCloudTrace = "1.2.12"
     val grpc = "1.36.0"
     val http4s = "0.21.19"
