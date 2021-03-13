19ce04f0b9e17939cd23a14d1575506c0bea9f40
catostrophe <40268503+catostrophe@users.noreply.github.com>
2021-03-13 09:57:18 +0300

    test Avro exporter with multiple fibers

--- modules/avro-test/src/test/scala/io/janstenpickle/trace4cats/avro/test/AvroServerSpec.scala
+++ modules/avro-test/src/test/scala/io/janstenpickle/trace4cats/avro/test/AvroServerSpec.scala
@@ -4,6 +4,7 @@ import cats.data.NonEmptyList
 import cats.effect.{Blocker, IO, Resource}
 import cats.kernel.Eq
 import cats.syntax.all._
+import fs2.Chunk
 import fs2.concurrent.Queue
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
@@ -12,7 +13,7 @@ import io.janstenpickle.trace4cats.avro.{AvroSpanCompleter, AvroSpanExporter}
 import io.janstenpickle.trace4cats.avro.server.AvroServer
 import io.janstenpickle.trace4cats.model.{Batch, CompletedSpan, TraceProcess}
 import io.janstenpickle.trace4cats.test.ArbitraryInstances
-import org.scalacheck.Shrink
+import org.scalacheck.{Arbitrary, Gen, Shrink}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
@@ -30,12 +31,17 @@ class AvroServerSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks wit
   implicit override val generatorDrivenConfig: PropertyCheckConfiguration =
     PropertyCheckConfiguration(minSuccessful = 1, maxDiscardedFactor = 50.0)
 
+  implicit val nBatchesArbitrary: Arbitrary[(Int, List[Batch[Chunk]])] = Arbitrary(for {
+    n <- Gen.choose(2, 4)
+    list <- Gen.listOfN(n, batchArb.arbitrary)
+  } yield (n, list))
+
   implicit def noShrink[T]: Shrink[T] = Shrink.shrinkAny
 
   behavior.of("Avro TCP")
 
   it should "send batches" in {
-    forAll { batch: Batch[List] =>
+    forAll { batch: Batch[Chunk] =>
       Queue
         .unbounded[IO, CompletedSpan]
         .flatMap { queue =>
@@ -43,13 +49,36 @@ class AvroServerSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks wit
             server <- AvroServer.tcp[IO](blocker, queue.enqueue)
             _ <- server.compile.drain.background
             _ <- Resource.liftF(timer.sleep(2.seconds))
-            completer <- AvroSpanExporter.tcp[IO, List](blocker)
+            completer <- AvroSpanExporter.tcp[IO, Chunk](blocker)
           } yield completer).use(_.exportBatch(batch) >> timer.sleep(3.seconds)) >> queue.dequeue
             .take(batch.spans.size.toLong)
             .compile
-            .toList
+            .to(Chunk)
             .map { spans =>
               Eq.eqv(spans, batch.spans)
+            }
+        }
+        .unsafeRunSync()
+    }
+  }
+
+  it should "send batches via multiple fibers" in {
+    forAll { nBatches: (Int, List[Batch[Chunk]]) =>
+      val (n, batches) = nBatches
+      Queue
+        .unbounded[IO, CompletedSpan]
+        .flatMap { queue =>
+          (for {
+            server <- AvroServer.tcp[IO](blocker, queue.enqueue)
+            _ <- server.compile.drain.background
+            _ <- Resource.liftF(timer.sleep(2.seconds))
+            completer <- AvroSpanExporter.tcp[IO, Chunk](blocker, numFibers = n)
+          } yield completer).use(c => batches.traverse_(c.exportBatch) >> timer.sleep(3.seconds)) >> queue.dequeue
+            .take(batches.foldMap(_.spans.size.toLong))
+            .compile
+            .toList
+            .map { spans =>
+              Eq.eqv(spans.sortBy(_.context.spanId.show), batches.flatMap(_.spans.toList).sortBy(_.context.spanId.show))
             }
         }
         .unsafeRunSync()
@@ -87,7 +116,7 @@ class AvroServerSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks wit
   behavior.of("Avro UDP")
 
   it should "send batches" in {
-    forAll { batch: Batch[List] =>
+    forAll { batch: Batch[Chunk] =>
       Queue
         .unbounded[IO, CompletedSpan]
         .flatMap { queue =>
@@ -95,13 +124,36 @@ class AvroServerSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks wit
             server <- AvroServer.udp[IO](blocker, queue.enqueue, port = 7779)
             _ <- server.compile.drain.background
             _ <- Resource.liftF(timer.sleep(2.seconds))
-            completer <- AvroSpanExporter.udp[IO, List](blocker, port = 7779)
+            completer <- AvroSpanExporter.udp[IO, Chunk](blocker, port = 7779)
           } yield completer).use(_.exportBatch(batch) >> timer.sleep(3.seconds)) >> queue.dequeue
             .take(batch.spans.size.toLong)
             .compile
-            .toList
+            .to(Chunk)
             .map { spans =>
               Eq.eqv(spans, batch.spans)
+            }
+        }
+        .unsafeRunSync()
+    }
+  }
+
+  it should "send batches via multiple fibers" in {
+    forAll { nBatches: (Int, List[Batch[Chunk]]) =>
+      val (n, batches) = nBatches
+      Queue
+        .unbounded[IO, CompletedSpan]
+        .flatMap { queue =>
+          (for {
+            server <- AvroServer.udp[IO](blocker, queue.enqueue, port = 7781)
+            _ <- server.compile.drain.background
+            _ <- Resource.liftF(timer.sleep(2.seconds))
+            completer <- AvroSpanExporter.udp[IO, Chunk](blocker, port = 7781, numFibers = n)
+          } yield completer).use(c => batches.traverse_(c.exportBatch) >> timer.sleep(3.seconds)) >> queue.dequeue
+            .take(batches.foldMap(_.spans.size.toLong))
+            .compile
+            .toList
+            .map { spans =>
+              Eq.eqv(spans.sortBy(_.context.spanId.show), batches.flatMap(_.spans.toList).sortBy(_.context.spanId.show))
             }
         }
         .unsafeRunSync()
