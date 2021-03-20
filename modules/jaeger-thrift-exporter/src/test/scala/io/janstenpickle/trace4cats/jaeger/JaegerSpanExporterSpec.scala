088de184135b76ca830fb6ca4da6dcda4ce35f70
catostrophe <40268503+catostrophe@users.noreply.github.com>
2021-03-20 20:56:54 +0300

    fix fluky jaeger tests

--- modules/jaeger-thrift-exporter/src/test/scala/io/janstenpickle/trace4cats/jaeger/JaegerSpanExporterSpec.scala
+++ modules/jaeger-thrift-exporter/src/test/scala/io/janstenpickle/trace4cats/jaeger/JaegerSpanExporterSpec.scala
@@ -15,7 +15,7 @@ class JaegerSpanExporterSpec extends BaseJaegerSpec {
         batch.spans.map(span =>
           span.copy(
             serviceName = process.serviceName,
-            attributes = process.attributes ++ span.attributes,
+            attributes = (process.attributes ++ span.attributes).filterNot { case (key, _) => key == "ip" },
             start = Instant.now(),
             end = Instant.now()
           )
