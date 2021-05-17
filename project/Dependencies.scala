bdc456bab0e12cd5573656d4a9fcd76a7b916b7e
Scala Steward <me@scala-steward.org>
2021-05-17 23:22:16 +0200

    Update log4cats to 1.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
     val jwt = "3.16.0"
     val kafka = "2.8.0"
     val kittens = "2.3.0"
-    val log4cats = "1.3.0"
+    val log4cats = "1.3.1"
     val logback = "1.2.3"
     val micronaut = "2.5.3"
     val natchez = "0.0.24"
