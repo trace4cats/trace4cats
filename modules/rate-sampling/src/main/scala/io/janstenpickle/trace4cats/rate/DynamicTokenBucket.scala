daafa77e80e7bfd301ca173f9aff4ef22ad666ea
Chris Jansen <janstenpickle@users.noreply.github.com>
2021-05-19 21:51:47 +0100

    Update modules/rate-sampling/src/main/scala/io/janstenpickle/trace4cats/rate/DynamicTokenBucket.scala
    Co-authored-by: catostrophe <40268503+catostrophe@users.noreply.github.com>

--- modules/rate-sampling/src/main/scala/io/janstenpickle/trace4cats/rate/DynamicTokenBucket.scala
+++ modules/rate-sampling/src/main/scala/io/janstenpickle/trace4cats/rate/DynamicTokenBucket.scala
@@ -3,7 +3,8 @@ package io.janstenpickle.trace4cats.rate
 import cats.Applicative
 import cats.effect.kernel.Ref
 import cats.effect.kernel.syntax.spawn._
-import cats.effect.{Resource, Temporal}
+import cats.effect.std.Hotswap
+import cats.effect.kernel.{Resource, Temporal}
 import cats.syntax.flatMap._
 import cats.syntax.functor._
 
@@ -17,41 +18,22 @@ object DynamicTokenBucket {
   type CancelToken[F[_]] = F[Unit]
 
   def create[F[_]: Temporal](bucketSize: Int, tokenRate: FiniteDuration): Resource[F, DynamicTokenBucket[F]] =
-    Resource.make(unsafeCreate(bucketSize, tokenRate))(_._2).map(_._1)
-
-  def unsafeCreate[F[_]: Temporal](
-    bucketSize: Int,
-    tokenRate: FiniteDuration
-  ): F[(DynamicTokenBucket[F], CancelToken[F])] =
     for {
-      currentConfig <- Ref.of[F, ((Int, FiniteDuration), Boolean)](((bucketSize, tokenRate), false))
-      tokens <- Ref.of(bucketSize)
-      processFiber <- TokenBucket.bucketProcess(tokens, bucketSize, tokenRate).start
+      currentConfig <- Resource.eval(Ref.of[F, (Int, FiniteDuration)]((bucketSize, tokenRate)))
+      tokens <- Resource.eval(Ref.of(bucketSize))
+      (hotswap, _) <- Hotswap(TokenBucket.bucketProcess(tokens, bucketSize, tokenRate).background.void)
+    } yield new DynamicTokenBucket[F] {
+      private final val underlying = TokenBucket.impl[F](tokens)
 
-      processFiberRef <- Ref.of(processFiber)
-    } yield (
-      new DynamicTokenBucket[F] {
-        private final val underlying = TokenBucket.impl[F](tokens)
-
-        override def updateConfig(bucketSize: Int, tokenRate: FiniteDuration): F[Unit] =
-          currentConfig.get.flatMap { case (conf, isCanceled) =>
-            if (conf != (bucketSize -> tokenRate) && !isCanceled) for {
-              processFiber <- processFiberRef.get
-              _ <- processFiber.cancel
-              newProcessFiber <- TokenBucket.bucketProcess(tokens, bucketSize, tokenRate).start
-              _ <- processFiberRef.set(newProcessFiber)
-            } yield ()
-            else Applicative[F].unit
+      override def updateConfig(bucketSize: Int, tokenRate: FiniteDuration): F[Unit] =
+        currentConfig.get.flatMap { oldConfig =>
+          Applicative[F].whenA(oldConfig != (bucketSize -> tokenRate)) {
+            hotswap.swap(TokenBucket.bucketProcess(tokens, bucketSize, tokenRate).background.void)
           }
+        }
 
-        override def request1: F[Boolean] = underlying.request1
+      override def request1: F[Boolean] = underlying.request1
 
-        override def request(n: Int): F[Int] = underlying.request(n)
-      },
-      for {
-        fiber <- processFiberRef.get
-        _ <- currentConfig.update { case (conf, _) => (conf, true) }
-        _ <- fiber.cancel
-      } yield ()
-    )
+      override def request(n: Int): F[Int] = underlying.request(n)
+    }
 }
