93fb764cc6d45ee8813bb58562d4de1fe7cf2d63
Chris Jansen <janstenpickle@users.noreply.github.com>
2021-05-19 21:50:39 +0100

    Update modules/dynamic-sampling/src/main/scala/io/janstenpickle/trace4cats/sampling/dynamic/SamplerUtil.scala
    Co-authored-by: catostrophe <40268503+catostrophe@users.noreply.github.com>

--- modules/dynamic-sampling/src/main/scala/io/janstenpickle/trace4cats/sampling/dynamic/SamplerUtil.scala
+++ modules/dynamic-sampling/src/main/scala/io/janstenpickle/trace4cats/sampling/dynamic/SamplerUtil.scala
@@ -1,26 +1,23 @@
 package io.janstenpickle.trace4cats.sampling.dynamic
 
-import cats.effect.kernel.Temporal
+import cats.effect.kernel.{Resource, Temporal}
 import cats.syntax.applicative._
-import cats.syntax.functor._
 import io.janstenpickle.trace4cats.kernel.SpanSampler
 import io.janstenpickle.trace4cats.rate.sampling.RateSpanSampler
 import io.janstenpickle.trace4cats.rate.{DynamicTokenBucket, TokenInterval}
 
 object SamplerUtil {
-  private[dynamic] def makeSampler[F[_]: Temporal](config: SamplerConfig): F[SamplerProcess[F]] = config match {
-    case SamplerConfig.Always => SamplerProcess(SpanSampler.always[F], ().pure).pure
-    case SamplerConfig.Never => SamplerProcess(SpanSampler.never[F], ().pure).pure
-    case SamplerConfig.Probabilistic(probability, rootSpansOnly) =>
-      SamplerProcess(SpanSampler.probabilistic[F](probability, rootSpansOnly), ().pure).pure
-    case SamplerConfig.Rate(bucketSize, tokenRate, rootSpansOnly) =>
-      TokenInterval(tokenRate)
-        .fold(SamplerProcess(SpanSampler.always[F], ().pure).pure)(interval =>
-          DynamicTokenBucket.unsafeCreate[F](bucketSize, interval).map { case (tb, cancel) =>
-            implicit val tokenBucket: DynamicTokenBucket[F] = tb
-
-            SamplerProcess(RateSpanSampler[F](rootSpansOnly), cancel)
-          }
-        )
+  private[dynamic] def makeSampler[F[_]: Temporal](config: SamplerConfig): Resource[F, SpanSampler[F]] = {
+    type R[A] = Resource[F, A]
+    config match {
+      case SamplerConfig.Always => SpanSampler.always[F].pure[R]
+      case SamplerConfig.Never => SpanSampler.never[F].pure[R]
+      case SamplerConfig.Probabilistic(probability, rootSpansOnly) =>
+        SpanSampler.probabilistic[F](probability, rootSpansOnly).pure[R]
+      case SamplerConfig.Rate(bucketSize, tokenRate, rootSpansOnly) =>
+        TokenInterval(tokenRate).fold(SpanSampler.always[F].pure[R]) { interval =>
+          DynamicTokenBucket.create[F](bucketSize, interval).map(implicit tb => RateSpanSampler[F](rootSpansOnly))
+        }
+    }
   }
 }
