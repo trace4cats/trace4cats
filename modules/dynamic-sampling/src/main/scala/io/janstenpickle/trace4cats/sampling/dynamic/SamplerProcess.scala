ddb920cc6c86a5e78d1fcecc0ace5a1213a6b87a
Chris Jansen <janstenpickle@users.noreply.github.com>
2021-05-19 21:50:14 +0100

    Update modules/dynamic-sampling/src/main/scala/io/janstenpickle/trace4cats/sampling/dynamic/SamplerProcess.scala
    Co-authored-by: catostrophe <40268503+catostrophe@users.noreply.github.com>

--- modules/dynamic-sampling/src/main/scala/io/janstenpickle/trace4cats/sampling/dynamic/SamplerProcess.scala
+++ modules/dynamic-sampling/src/main/scala/io/janstenpickle/trace4cats/sampling/dynamic/SamplerProcess.scala
@@ -1,5 +1,1 @@
-package io.janstenpickle.trace4cats.sampling.dynamic
 
-import io.janstenpickle.trace4cats.kernel.SpanSampler
-
-private[dynamic] case class SamplerProcess[F[_]](value: SpanSampler[F], cancel: F[Unit])
