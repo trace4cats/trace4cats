2f8f7b4c15f8e4f062940a4911d59c244a9889d4
Scala Steward <me@scala-steward.org>
2021-05-21 17:35:28 +0300

    Update kittens to 2.3.2
    (cherry picked from commit 32786ffffbb801ef187d3252e59428c815af894d)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
     val jaeger = "1.6.0"
     val jwt = "3.16.0"
     val kafka = "2.8.0"
-    val kittens = "2.3.0"
+    val kittens = "2.3.2"
     val log4cats = "1.3.1"
     val logback = "1.2.3"
     val micronaut = "2.5.4"
