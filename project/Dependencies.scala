1b654596c8571a39884b930884aeaf2cde47d1de
Scala Steward <me@scala-steward.org>
2021-05-27 01:21:21 +0200

    Update natchez-core to 0.1.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,7 +26,7 @@ object Dependencies {
     val log4cats = "2.1.1"
     val logback = "1.2.3"
     val micronaut = "2.5.4"
-    val natchez = "0.1.4"
+    val natchez = "0.1.5"
     val openTelemetry = "1.2.0"
     val redis4cats = "1.0.0-RC3"
     val scaffeine = "4.0.2"
