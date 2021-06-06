93b37aaf35eed23d3f09cfd0b5e8507d47be356a
catostrophe <40268503+catostrophe@users.noreply.github.com>
2021-06-06 16:50:13 +0300

    relocate http4s and sttp modules to separate repos

--- modules/example/src/main/scala/io/janstenpickle/trace4cats/example/SttpExample.scala
+++ modules/example/src/main/scala/io/janstenpickle/trace4cats/example/SttpExample.scala
@@ -1,19 +1,21 @@
-package io.janstenpickle.trace4cats.example
+// TODO move to separate example repo
 
-import cats.effect.{ExitCode, IO, IOApp, Resource}
-import io.janstenpickle.trace4cats.sttp.client3.syntax._
-import org.http4s.blaze.client.BlazeClientBuilder
-import sttp.client3.http4s.Http4sBackend
-
-object SttpExample extends IOApp {
-
-  override def run(args: List[String]): IO[ExitCode] =
-    (for {
-      ec <- Resource.eval(IO.executionContext)
-      client <- BlazeClientBuilder[IO](ec).resource
-      sttpBackend = Http4sBackend.usingClient(client)
-      tracedBackend = sttpBackend.liftTrace()
-    } yield tracedBackend).use { _ =>
-      IO(ExitCode.Success)
-    }
-}
+//package io.janstenpickle.trace4cats.example
+//
+//import cats.effect.{ExitCode, IO, IOApp, Resource}
+//import io.janstenpickle.trace4cats.sttp.client3.syntax._
+//import org.http4s.blaze.client.BlazeClientBuilder
+//import sttp.client3.http4s.Http4sBackend
+//
+//object SttpExample extends IOApp {
+//
+//  override def run(args: List[String]): IO[ExitCode] =
+//    (for {
+//      ec <- Resource.eval(IO.executionContext)
+//      client <- BlazeClientBuilder[IO](ec).resource
+//      sttpBackend = Http4sBackend.usingClient(client)
+//      tracedBackend = sttpBackend.liftTrace()
+//    } yield tracedBackend).use { _ =>
+//      IO(ExitCode.Success)
+//    }
+//}
