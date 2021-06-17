2731e6b0563de0625446e4e019b5af3a2dceaa21
catostrophe <40268503+catostrophe@users.noreply.github.com>
2021-06-17 14:05:07 +0300

    Update scalapb-json4s to 0.11.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -32,7 +32,7 @@ object Dependencies {
     val scaffeine = "4.0.2"
     val scala212 = "2.12.14"
     val scala213 = "2.13.6"
-    val scalapb = "0.11.0"
+    val scalapb = "0.11.1"
     val sttpClient2 = "2.2.9"
     val sttpClient3 = "3.3.5"
     val sttpModel = "1.4.7"
