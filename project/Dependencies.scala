25bbf3ae107c1cd03bbc86649a5dcd3fd3b07bb9
catostrophe <40268503+catostrophe@users.noreply.github.com>
2021-06-17 14:07:26 +0300

    Update google-cloud-trace to 1.4.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     val fs2 = "2.5.6"
     val fs2Kafka = "1.7.0"
     val googleCredentials = "0.26.0"
-    val googleCloudTrace = "1.4.0"
+    val googleCloudTrace = "1.4.1"
     val grpc = "1.38.0"
     val http4s = "0.21.24"
     val http4sJdkClient = "0.3.7"
