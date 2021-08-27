dfc2c7ce68d4c98af616379af311f1ff97e40d85
Scala Steward <me@scala-steward.org>
2021-08-27 05:16:05 +0200

    Update kind-projector to 0.13.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
     val scalaCheckShapeless = "1.3.0"
     val scalaTest = "3.2.9"
 
-    val kindProjector = "0.13.0"
+    val kindProjector = "0.13.1"
     val betterMonadicFor = "0.3.1"
   }
 
