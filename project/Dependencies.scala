55a55998ade6bc993301f6398648d1c0e28f1aa3
Scala Steward <me@scala-steward.org>
2021-10-20 03:17:01 +0200

    Update discipline-core to 1.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
 
     val catsTestkitScalatest = "2.1.5"
     val disciplineScalatest = "2.1.5"
-    val discipline = "1.1.5"
+    val discipline = "1.2.0"
     val scalaCheck = "1.15.4"
     val scalaCheckShapeless = "1.3.0"
     val scalaTest = "3.2.10"
