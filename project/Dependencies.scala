89686081b425ee270391da3a62966d4d71e273a0
Scala Steward <me@scala-steward.org>
2021-11-28 10:29:11 +0100

    Update cats-core, cats-laws to 2.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   object Versions {
     val caseInsensitive = "1.2.0"
-    val cats = "2.6.1"
+    val cats = "2.7.0"
     val catsEffect = "3.2.9"
     val collectionCompat = "2.6.0"
     val commonsCodec = "1.15"
