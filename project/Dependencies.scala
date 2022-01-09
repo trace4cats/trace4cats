aea2f444027890b4c8296b8ffb11b64c28251ada
Scala Steward <me@scala-steward.org>
2022-01-09 06:45:10 +0100

    Update cats-effect, cats-effect-kernel, ... to 3.3.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   object Versions {
     val caseInsensitive = "1.2.0"
     val cats = "2.7.0"
-    val catsEffect = "3.3.3"
+    val catsEffect = "3.3.4"
     val collectionCompat = "2.6.0"
     val commonsCodec = "1.15"
     val fs2 = "3.2.4"
