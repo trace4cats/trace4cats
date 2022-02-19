84b48d766396f4b04f1a6298525175afdb117aea
Scala Steward <me@scala-steward.org>
2022-02-19 19:08:33 +0100

    Update fs2-core to 3.2.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val catsEffect = "3.3.5"
     val collectionCompat = "2.6.0"
     val commonsCodec = "1.15"
-    val fs2 = "3.2.4"
+    val fs2 = "3.2.5"
     val hotswapRef = "0.2.1"
     val kittens = "2.3.2"
     val log4cats = "2.2.0"
