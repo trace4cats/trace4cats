5785edea953fbfda4e831c8819ec45452a41743a
catostrophe <40268503+catostrophe@users.noreply.github.com>
2022-04-16 11:26:15 +0300

    fix: rework cats-effect-io module
    1. Change bounds on F in DischargeSpan
    2. Replace LocalForIOLocal with ProvideForIOLocal
    3. Simplify instance providers in package object

--- modules/cats-effect-io/src/main/scala/io/janstenpickle/trace4cats/cats/effect/package.scala
+++ modules/cats-effect-io/src/main/scala/io/janstenpickle/trace4cats/cats/effect/package.scala
@@ -1,39 +1,37 @@
 package io.janstenpickle.trace4cats.cats
 
 import cats.effect.{IO, IOLocal}
-import cats.~>
 import io.janstenpickle.trace4cats.Span
-import io.janstenpickle.trace4cats.base.context.Unlift.IdUnlift
 import io.janstenpickle.trace4cats.base.context.{Local, Provide}
 import io.janstenpickle.trace4cats.base.optics.Lens
 import io.janstenpickle.trace4cats.inject.Trace
 
 package object effect {
 
-  /** Construct a `Trace` backed by the given `IOLocal[Span[IO]]` */
-  def ioLocalTrace(rootSpan: IOLocal[Span[IO]]): Trace[IO] = {
-    implicit val local: Local[IO, Span[IO]] = new LocalForIOLocal(rootSpan)
-    Trace.localSpanInstance[IO, IO]
-  }
-
-  def ioLocalTraceWithContext[Ctx](rootCtx: IOLocal[Ctx])(implicit lens: Lens[Ctx, Span[IO]]): Trace[IO] = {
-    implicit val local: Local[IO, Span[IO]] = new LocalForIOLocal(rootCtx).focus(lens)
-    Trace.localSpanInstance[IO, IO]
-  }
-
   def ioLocalProvide(rootSpan: IOLocal[Span[IO]]): Provide[IO, IO, Span[IO]] =
-    new LocalForIOLocal(rootSpan) with Provide[IO, IO, Span[IO]] with IdUnlift[IO] {
-      override def provide[A](fa: IO[A])(r: Span[IO]): IO[A] = local(fa)(_ => r)
-      override def askUnlift: IO[IO ~> IO] = super.askUnlift
-    }
+    new ProvideForIOLocal(rootSpan)
 
   def ioLocalProvideWithContext[Ctx](rootCtx: IOLocal[Ctx]): Provide[IO, IO, Ctx] =
-    new LocalForIOLocal(rootCtx) with Provide[IO, IO, Ctx] with IdUnlift[IO] {
-      override def provide[A](fa: IO[A])(r: Ctx): IO[A] = local(fa)(_ => r)
-      override def askUnlift: IO[IO ~> IO] = super.askUnlift
-    }
+    new ProvideForIOLocal(rootCtx)
 
-  /** Construct a `Trace` backed by an `IOLocal[Span[IO]]`, using the given span as the initial value */
+  /** Construct a `Trace` backed by the given `IOLocal[Span[IO]]`. */
+  def ioLocalTrace(rootSpan: IOLocal[Span[IO]]): Trace[IO] = {
+    implicit val local: Local[IO, Span[IO]] = ioLocalProvide(rootSpan)
+    Trace[IO]
+  }
+
+  /** Construct a `Trace` backed by an `IOLocal[Span[IO]]`, using the given span as the initial value. */
   def ioLocalTrace(rootSpan: Span[IO]): IO[Trace[IO]] =
     IOLocal(rootSpan).map(ioLocalTrace)
+
+  /** Construct a `Trace` backed by the given `IOLocal[Ctx]`. */
+  def ioLocalTraceWithContext[Ctx](rootCtx: IOLocal[Ctx])(implicit lens: Lens[Ctx, Span[IO]]): Trace[IO] = {
+    implicit val local: Local[IO, Span[IO]] = ioLocalProvideWithContext(rootCtx).focus(lens)
+    Trace[IO]
+  }
+
+  /** Construct a `Trace` backed by an `IOLocal[Ctx]`, using the given context as the initial value. */
+  def ioLocalTraceWithContext[Ctx](rootCtx: Ctx)(implicit lens: Lens[Ctx, Span[IO]]): IO[Trace[IO]] =
+    IOLocal(rootCtx).map(ioLocalTraceWithContext(_))
+
 }
