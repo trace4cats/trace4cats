5a8b7b9a5b829edd0dda0d822ff60ee543fdbb6e
janstenpickle <chris.jansen@permutive.com>
2022-04-26 12:37:42 +0100

    Add `Trace.WithContext`
    Adds the ability to obtain a span context from a `Trace` typeclass.
    This has been added as a more powerful typeclass so compatibility
    with Natchez can be preserved.

--- modules/inject/src/test/scala/io/janstenpickle/trace4cats/inject/KleisliTraceInstanceSummonTest.scala
+++ modules/inject/src/test/scala/io/janstenpickle/trace4cats/inject/KleisliTraceInstanceSummonTest.scala
@@ -8,8 +8,10 @@ import io.janstenpickle.trace4cats.base.context.Local
 object KleisliTraceInstanceSummonTest {
   type F[x] = Kleisli[IO, Span[IO], x]
   implicitly[Trace[F]]
+  implicitly[Trace.WithContext[F]]
 
   type G[x] = Kleisli[IO, Env[IO], x]
   implicit val gHasLocalSpan: Local[G, Span[IO]] = Local[G, Env[IO]].focus(Env.span)
   implicitly[Trace[G]]
+  implicitly[Trace.WithContext[G]]
 }
