efb9b7b4147ea9e4db67bb89f4537d272898df07
Chris Jansen <janstenpickle@users.noreply.github.com>
2022-05-22 13:22:16 +0100

    Fix 2.12 compile error

--- modules/fs2/src/main/scala/trace4cats/fs2/fs2.scala
+++ modules/fs2/src/main/scala/trace4cats/fs2/fs2.scala
@@ -2,7 +2,6 @@ package trace4cats
 
 import _root_.fs2.Stream
 import cats.data.WriterT
-import trace4cats.kernel.Span
 
 package object fs2 {
   type TracedStream[F[_], A] = WriterT[Stream[F, *], Span[F], A]
