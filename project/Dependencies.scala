fe7e5a3b5ffdaf9e359a5648c69326aa6cfe3c5d
Scala Steward <scala_steward@virtuslab.com>
2022-07-19 18:48:37 +0000

    Revert commit(s) 66b61ca6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
     val caseInsensitive = "1.2.0"
     val cats = "2.8.0"
     val catsEffect = "3.3.13"
-    val collectionCompat = "2.8.0"
+    val collectionCompat = "2.7.0"
     val commonsCodec = "1.15"
     val fs2 = "3.2.9"
     val hotswapRef = "0.2.2"
