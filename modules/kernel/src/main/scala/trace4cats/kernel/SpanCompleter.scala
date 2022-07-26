b399a5a29d0e2c38bb7dd8c404e7cddf7d48ccd0
Chris Jansen <janstenpickle@users.noreply.github.com>
2022-07-26 21:55:58 +0100

    Remove monoids
    Monoid instances for span completer and exporters weren't functioning
    properly, so I am removing them in favour of `combined` methods

--- modules/kernel/src/main/scala/trace4cats/kernel/SpanCompleter.scala
+++ modules/kernel/src/main/scala/trace4cats/kernel/SpanCompleter.scala
@@ -1,7 +1,7 @@
 package trace4cats.kernel
 
-import cats.kernel.Monoid
-import cats.{Applicative, Apply, Parallel}
+import cats.syntax.parallel._
+import cats.{Applicative, Parallel}
 import trace4cats.model.CompletedSpan
 import trace4cats.model.CompletedSpan.Builder
 
@@ -9,33 +9,13 @@ trait SpanCompleter[F[_]] {
   def complete(span: CompletedSpan.Builder): F[Unit]
 }
 
-object SpanCompleter extends LowPrioritySpanCompleterInstances {
-  implicit def spanCompleterMonoidFromParallel[F[_]: Applicative: Parallel]: Monoid[SpanCompleter[F]] =
-    new Monoid[SpanCompleter[F]] {
-      override def combine(x: SpanCompleter[F], y: SpanCompleter[F]): SpanCompleter[F] =
-        new SpanCompleter[F] {
-          override def complete(span: CompletedSpan.Builder): F[Unit] =
-            Parallel.parMap2(x.complete(span), y.complete(span))((_, _) => ())
-        }
-
-      override def empty: SpanCompleter[F] = SpanCompleter.empty[F]
-    }
-}
-
-trait LowPrioritySpanCompleterInstances {
-  implicit def spanCompleterMonoidFromApply[F[_]: Applicative]: Monoid[SpanCompleter[F]] =
-    new Monoid[SpanCompleter[F]] {
-      override def combine(x: SpanCompleter[F], y: SpanCompleter[F]): SpanCompleter[F] =
-        new SpanCompleter[F] {
-          override def complete(span: Builder): F[Unit] =
-            Apply[F].map2(x.complete(span), y.complete(span))((_, _) => ())
-        }
-
-      override def empty: SpanCompleter[F] = SpanCompleter.empty[F]
-    }
-
+object SpanCompleter {
   def empty[F[_]: Applicative]: SpanCompleter[F] =
     new SpanCompleter[F] {
       override def complete(span: CompletedSpan.Builder): F[Unit] = Applicative[F].unit
     }
+
+  def combined[F[_]: Parallel](completers: List[SpanCompleter[F]]): SpanCompleter[F] = new SpanCompleter[F] {
+    override def complete(span: Builder): F[Unit] = completers.parTraverse_(_.complete(span))
+  }
 }
