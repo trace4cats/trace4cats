cd567d15d3ca64e11dd60d6af7cec90e4377ffe1
Chris Jansen <janstenpickle@users.noreply.github.com>
2022-11-09 17:19:55 +0000

    Rework span queued span completer
    The queued span completer was causing contention around the `Ref` for
    recording in flight requests. Seeing as this was only really used to
    ensure that the completer can be shut down properly, there's a better
    way of doing this new we have CE3.

--- modules/core/src/main/scala/trace4cats/QueuedSpanCompleter.scala
+++ modules/core/src/main/scala/trace4cats/QueuedSpanCompleter.scala
@@ -1,18 +1,14 @@
 package trace4cats
 
 import cats.Applicative
-import cats.effect.kernel.syntax.monadCancel._
 import cats.effect.kernel.syntax.spawn._
-import cats.effect.kernel.{Clock, Ref, Resource, Temporal}
+import cats.effect.kernel.{Ref, Resource, Temporal}
 import cats.effect.std.Queue
 import cats.syntax.applicativeError._
 import cats.syntax.flatMap._
 import cats.syntax.functor._
-import cats.syntax.monad._
 import fs2.{Chunk, Stream}
 import org.typelevel.log4cats.Logger
-
-import scala.concurrent.duration._
 
 object QueuedSpanCompleter {
   def apply[F[_]: Temporal: Logger](
@@ -22,49 +18,49 @@ object QueuedSpanCompleter {
   ): Resource[F, SpanCompleter[F]] = {
     val realBufferSize = if (config.bufferSize < config.batchSize * 5) config.batchSize * 5 else config.bufferSize
 
+    def exportBatches(stream: Stream[F, CompletedSpan]): F[Unit] =
+      stream
+        .groupWithin(config.batchSize, config.batchTimeout)
+        .evalMap { spans =>
+          Stream
+            .retry(
+              exporter.exportBatch(Batch(spans)),
+              delay = config.retryConfig.delay,
+              nextDelay = config.retryConfig.nextDelay.calc,
+              maxAttempts = config.retryConfig.maxAttempts
+            )
+            .compile
+            .drain
+            .onError { case th =>
+              Logger[F].warn(th)("Failed to export spans")
+            }
+        }
+        .compile
+        .drain
+
     for {
-      inFlight <- Resource.eval(Ref.of(0))
       hasLoggedWarn <- Resource.eval(Ref.of(false))
       queue <- Resource.eval(Queue.bounded[F, CompletedSpan](realBufferSize))
-      _ <- Resource.make {
-        Stream
-          .fromQueueUnterminated(queue)
-          .groupWithin(config.batchSize, config.batchTimeout)
-          .map(spans => Batch(spans))
-          .evalMap { batch =>
-            Stream
-              .retry(
-                exporter.exportBatch(batch),
-                delay = config.retryConfig.delay,
-                nextDelay = config.retryConfig.nextDelay.calc,
-                maxAttempts = config.retryConfig.maxAttempts
-              )
-              .compile
-              .drain
-              .onError { case th =>
-                Logger[F].warn(th)("Failed to export spans")
-              }
-              .guarantee(inFlight.update(_ - batch.spans.size))
-          }
-          .compile
-          .drain
-          .start
-      }(fiber => Clock[F].sleep(50.millis).whileM_(inFlight.get.map(_ != 0)) >> fiber.cancel)
+      _ <- exportBatches(Stream.fromQueueUnterminated(queue)).background.onFinalize(
+        exportBatches(
+          Stream
+            .repeatEval(queue.tryTake)
+            .unNoneTerminate
+        )
+      )
     } yield new SpanCompleter[F] {
       override def complete(span: CompletedSpan.Builder): F[Unit] = {
-        val enqueue = queue.offer(span.build(process)) >> inFlight.update { current =>
-          if (current == realBufferSize) current
-          else current + 1
-        }
 
-        val warnLog = hasLoggedWarn.get.ifM(
-          Logger[F].warn(s"Failed to enqueue new span, buffer is full of $realBufferSize") >> hasLoggedWarn.set(true),
-          Applicative[F].unit,
-        )
+        val warnLog = hasLoggedWarn.get
+          .map(!_)
+          .ifM(
+            Logger[F].warn(s"Failed to enqueue new span, buffer is full of $realBufferSize") >> hasLoggedWarn.set(true),
+            Applicative[F].unit,
+          )
 
-        inFlight.get
-          .map(_ == realBufferSize)
-          .ifM(warnLog, enqueue >> hasLoggedWarn.set(false))
+        queue
+          .tryOffer(span.build(process))
+          .ifM(hasLoggedWarn.set(false), warnLog)
       }
     }
   }
