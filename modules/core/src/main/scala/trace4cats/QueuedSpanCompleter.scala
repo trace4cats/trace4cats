df1b4e3a7f7dd49d8640982d7e5b1d74e748c2e9
Chris Jansen <janstenpickle@users.noreply.github.com>
2022-11-09 17:58:19 +0000

    Shut down queue gracefully

--- modules/core/src/main/scala/trace4cats/QueuedSpanCompleter.scala
+++ modules/core/src/main/scala/trace4cats/QueuedSpanCompleter.scala
@@ -2,12 +2,13 @@ package trace4cats
 
 import cats.Applicative
 import cats.effect.kernel.syntax.spawn._
-import cats.effect.kernel.{Ref, Resource, Temporal}
+import cats.effect.kernel.{Deferred, Ref, Resource, Temporal}
 import cats.effect.std.Queue
 import cats.syntax.applicativeError._
 import cats.syntax.flatMap._
 import cats.syntax.functor._
 import fs2.{Chunk, Stream}
+import cats.effect.kernel.syntax.monadCancel._
 import org.typelevel.log4cats.Logger
 
 object QueuedSpanCompleter {
@@ -18,7 +19,7 @@ object QueuedSpanCompleter {
   ): Resource[F, SpanCompleter[F]] = {
     val realBufferSize = if (config.bufferSize < config.batchSize * 5) config.batchSize * 5 else config.bufferSize
 
-    def exportBatches(stream: Stream[F, CompletedSpan]): F[Unit] =
+    def exportBatches(stream: Stream[F, CompletedSpan], signal: Deferred[F, Either[Throwable, Unit]]): F[Unit] =
       stream
         .groupWithin(config.batchSize, config.batchTimeout)
         .evalMap { spans =>
@@ -34,20 +35,27 @@ object QueuedSpanCompleter {
             .onError { case th =>
               Logger[F].warn(th)("Failed to export spans")
             }
+            .uncancelable
         }
+        .interruptWhen(signal)
         .compile
         .drain
 
     for {
       hasLoggedWarn <- Resource.eval(Ref.of(false))
+      shutdownSignal <- Resource.eval(Deferred[F, Either[Throwable, Unit]])
       queue <- Resource.eval(Queue.bounded[F, CompletedSpan](realBufferSize))
-      _ <- exportBatches(Stream.fromQueueUnterminated(queue)).background.onFinalize(
-        exportBatches(
-          Stream
-            .repeatEval(queue.tryTake)
-            .unNoneTerminate
+      _ <- exportBatches(Stream.fromQueueUnterminated(queue), shutdownSignal).uncancelable.background.onFinalize(
+        Deferred[F, Either[Throwable, Unit]].flatMap(
+          exportBatches(
+            Stream
+              .repeatEval(queue.tryTake)
+              .unNoneTerminate,
+            _
+          )
         )
       )
+      _ <- Resource.make(Applicative[F].unit)(_ => shutdownSignal.complete(Right(())).void)
     } yield new SpanCompleter[F] {
       override def complete(span: CompletedSpan.Builder): F[Unit] = {
 
