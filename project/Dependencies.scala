bfa56edbf61cd0710abafb15375b266f201872fc
Scala Steward <scala_steward@virtuslab.com>
2023-04-23 19:57:20 +0000

    Update log4cats-slf4j to 2.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val fs2 = "3.2.14"
     val hotswapRef = "0.2.2"
     val kittens = "2.3.2"
-    val log4cats = "2.4.0"
+    val log4cats = "2.6.0"
     val slf4j = "1.7.36"
     val scala212 = "2.12.16"
     val scala213 = "2.13.8"
