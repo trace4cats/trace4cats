471870765c6498e87fb8b82ba69e6355f17edf51
Scala Steward <scala_steward@virtuslab.com>
2023-06-30 03:26:02 +0000

    Update cats-effect-laws, cats-effect-std, ... to 3.5.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   object Versions {
     val caseInsensitive = "1.4.0"
     val cats = "2.9.0"
-    val catsEffect = "3.5.0"
+    val catsEffect = "3.5.1"
     val collectionCompat = "2.11.0"
     val commonsCodec = "1.15"
     val fs2 = "3.7.0"
