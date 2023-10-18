7b8fe6bf8def2551c73bd4df4642d9aae100b399
Alejandro Hernández <info@alejandrohdezma.com>
2023-10-18 13:00:50 +0200

    Add syntax for `Provide`

--- modules/context-utils/src/main/scala/trace4cats/context/syntax.scala
+++ modules/context-utils/src/main/scala/trace4cats/context/syntax.scala
@@ -12,4 +12,11 @@ object syntax {
 
   }
 
+  implicit class ProvideOps[F[_], A](val fa: F[A]) extends AnyVal {
+
+    def provide[Low[_], E](e: E)(implicit provide: Provide[Low, F, E]): Low[A] =
+      provide.provide(fa)(e)
+
+  }
+
 }
