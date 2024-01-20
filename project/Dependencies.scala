31586a98da9fee747dcc810fec44ac29491bae61
Scala Steward <scala_steward@virtuslab.com>
2024-01-20 20:00:25 +0000

    Update fs2-core to 3.9.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val catsEffect = "3.5.3"
     val collectionCompat = "2.11.0"
     val commonsCodec = "1.15"
-    val fs2 = "3.8.0"
+    val fs2 = "3.9.4"
     val hotswapRef = "0.2.2"
     val kittens = "2.3.2"
     val log4cats = "2.6.0"
